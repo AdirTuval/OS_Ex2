41c3e64ecd768b22a70190392ca7a36799570642
AdirTuval <adirtuval@gmail.com>
2022-04-01 18:39:45 +0300

    singleton working

--- uthreads.cpp
+++ uthreads.cpp
@@ -2,19 +2,29 @@
 // Created by adirt on 01/04/2022.
 //
 #include <cstdio>
+#include <cassert>
 #include "uthreads.h"
+#define NOT_SET -1
+class Scheduler{
+private:
+	explicit Scheduler(int quantum_usecs) : _quantum_usecs(quantum_usecs){}
+	Scheduler(Scheduler const&);              // Don't Implement
+	void operator=(Scheduler const&);
+	const int _quantum_usecs;
+public:
+	static Scheduler& getInstance(int quantum_usecs = 0){
+		static Scheduler instance(quantum_usecs);
+		return instance;
+	}
+	int getQuantumUsecs() const {return _quantum_usecs;}
+};
 
 /**
- * @brief initializes the thread library.
- *
- * You may assume that this function is called before any other thread library function, and that it is called
- * exactly once.
- * The input to the function is the length of a quantum in micro-seconds.
- * It is an error to call this function with non-positive quantum_usecs.
- *
- * @return On success, return 0. On failure, return -1.
-*/
-int uthread_init(int quantum_usecs);
+ * Global scheduler.
+ */
+int uthread_init(int quantum_usecs){
+
+}
 int uthread_spawn(thread_entry_point entry_point);
 int uthread_terminate(int tid);
 int uthread_block(int tid);
@@ -24,18 +34,11 @@ int uthread_get_tid();
 int uthread_get_total_quantums();
 int uthread_get_quantums(int tid);
 
-class Scheduler{
-private:
-	int _quantum_usecs;
-public:
-	Scheduler(int quantum_usecs) {
-		_quantum_usecs = quantum_usecs;
-	}
-};
-
-
 
 int main(){
-	printf("hello world!");
+	Scheduler &t = Scheduler::getInstance(20);
+	printf("%d", t.getQuantumUsecs());
+	Scheduler &c = Scheduler::getInstance(500);
+	printf("%d", c.getQuantumUsecs());
 	return 0;
 }
