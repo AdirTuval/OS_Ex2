bcd1913dd7f5fa52e7e1d6fc91654ca34604318f
AdirTuval <adirtuval@gmail.com>
2022-04-02 12:31:08 +0300

    aquarium

--- uthreads.cpp
+++ uthreads.cpp
@@ -5,25 +5,52 @@
 #include <cassert>
 #include "uthreads.h"
 #include <bits/stdc++.h>
+#include <csetjmp>
 #define FIRST_ID 0
 #define SUCCESS 0
 #define FAILURE -1
 using namespace std;
+#ifdef __x86_64__
+/* code for 64 bit Intel arch */
+
+typedef unsigned long address_t;
+#define JB_SP 6
+#define JB_PC 7
+
+/* A translation is required when using an address of a variable.
+   Use this as a black box in your code. */
+address_t translate_address(address_t addr)
+{
+	address_t ret;
+	asm volatile("xor    %%fs:0x30,%0\n"
+				 "rol    $0x11,%0\n"
+	: "=g" (ret)
+	: "0" (addr));
+	return ret;
+}
+#endif
 
 enum ThreadState {RUNNING, READY, BLOCKED, TERMINATED};
-
 
 class Thread{
 private:
 	ThreadState _state;
+	sigjmp_buf _env;
 	char * _stack;
 public:
-	Thread(){
+	Thread(thread_entry_point entry_point){
 		_state = ThreadState::READY;
 		_stack = new char[STACK_SIZE];
+		address_t sp = (address_t) _stack + STACK_SIZE - sizeof(address_t);
+		address_t pc = (address_t) entry_point;
+		sigsetjmp(_env, 1); // TODO check if savemask == 1
+		(_env->__jmpbuf)[JB_SP] = translate_address(sp);
+		(_env->__jmpbuf)[JB_PC] = translate_address(pc);
+		sigemptyset(_env->__saved_mask);
 	}
 	~Thread(){
-		printf("Destructed!");
+		delete _stack;
+		printf("Thread deleted");
 	}
 
 };
@@ -33,15 +60,15 @@ private:
 	explicit Scheduler(int quantum_usecs) : _quantum_usecs(quantum_usecs){
 		_used_threads_id.push(FIRST_ID);
 		_largest_thread_id = FIRST_ID;
-		_create_thread();
+//		_create_thread();
 	}
 	const int _quantum_usecs;
 	int _largest_thread_id;
 	priority_queue <int, vector<int>, greater<int>> _used_threads_id;
-	unordered_map<int, Thread> _active_threads;
-	queue<Thread> _ready_queue;
+	unordered_map<int, Thread *> _active_threads;
+	queue<Thread *> _ready_queue;
 public:
-	int _create_thread();
+	int _create_thread(thread_entry_point entry_point);
 	int _generate_thread_id();
 	Scheduler(Scheduler const&) = delete;
 	void operator=(Scheduler const&) = delete;
@@ -63,22 +90,20 @@ int Scheduler::_generate_thread_id()
 	return _largest_thread_id;
 }
 
-int Scheduler::_create_thread()
+int Scheduler::_create_thread(thread_entry_point entry_point)
 {
 	if(_active_threads.size() >= MAX_THREAD_NUM){
 		return FAILURE;
 	}
 	try {
 		int thread_id = _generate_thread_id();
-		Thread new_thread = Thread();
+		auto * new_thread = new Thread(entry_point);
 		_active_threads[thread_id] = new_thread;
 		_ready_queue.push(new_thread);
 		return thread_id;
 	} catch (bad_alloc&){
-
 		return FAILURE;
 	}
-
 }
 
 
@@ -90,7 +115,7 @@ int uthread_init(int quantum_usecs){
 	return SUCCESS;
 }
 int uthread_spawn(thread_entry_point entry_point){
-
+ //sigsetjump
 }
 int uthread_terminate(int tid){
 	Scheduler &scheduler = Scheduler::getInstance();
@@ -104,6 +129,6 @@ int uthread_get_quantums(int tid);
 
 int main(){
 	Scheduler &t = Scheduler::getInstance(20);
-	t._create_thread();
+//	t._create_thread();
 	return 0;
 }
